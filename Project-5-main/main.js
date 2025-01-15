0eae644855c98f9c95eeac2e1566fcb5bde8804c
MamdohSamer <143343786+MamdohSamer@users.noreply.github.com>
2025-01-15 07:12:29 +0200

    Add files via upload

---
+++ Project-5-main/main.js
@@ -0,0 +1,8 @@
+var number =0;
+var number = document.getElementById("ammount");
+console.log(number);
+const inc = document.getElementById("inc");
+const dec = document.getElementById("dec");
+inc.onclick=function(){
+    document.appendChild("div");
+}
